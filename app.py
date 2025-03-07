02a05721dd4a39f847e12e929c6cba34825a8283
Nibedita <nibeditamofficial@gmail.com>
2025-03-07 15:17:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('india.csv')
+
+list_of_states = list(df['State'].unique())
+list_of_states.insert(0, 'Overall India')
+
+st.sidebar.title('India Dataset Viz')
+
+st.sidebar.selectbox('Select a state', list_of_states)
+
